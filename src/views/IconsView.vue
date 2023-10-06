c21780bfb7c687c9fdde1834772ebc4be0e19438
Jisu-Woniu <31986081+Jisu-Woniu@users.noreply.github.com>
2023-10-07 00:15:21 +0800

    Update dependencies.

--- src/views/IconsView.vue
+++ src/views/IconsView.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
-import IconCommunity from "@/components/icons/IconCommunity.vue";
-import IconDocumentation from "@/components/icons/IconDocumentation.vue";
-import IconEcosystem from "@/components/icons/IconEcosystem.vue";
 import IconHeart from "@/components/icons/IconHeart.vue";
 import IconPick from "@/components/icons/IconPick.vue";
 import IconSast from "@/components/icons/IconSast.vue";
-import IconTooling from "@/components/icons/IconTooling.vue";
 </script>
 <template>
   <ul>
