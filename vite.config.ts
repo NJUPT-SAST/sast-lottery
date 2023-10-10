94c341609e5deff0dea28376829799e66e313c35
Jisu-Woniu <31986081+Jisu-Woniu@users.noreply.github.com>
2023-10-10 16:04:29 +0800

    Add testing PWA support.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import purgeCss from "@mojojoejo/vite-plugin-purgecss";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), purgeCss()],
+  plugins: [vue(), purgeCss(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
